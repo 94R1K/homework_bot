1fee05114fae9af8830adbb87b9f560f446f29ed
Ярослав Лошкарев <dfdfdf.dfdf.1985@mail.ru>
2022-08-13 19:51:09 +0300

    Ready!

--- homework.py
+++ homework.py
@@ -50,7 +50,7 @@ def send_message(bot, message):
 def get_api_answer(current_timestamp):
     """Запрос к эндпоинту и возвращение ответа API."""
     timestamp = current_timestamp or int(time.time())
-    params = {'from_date': 0}
+    params = {'from_date': timestamp}
     try:
         response = requests.get(ENDPOINT, headers=HEADERS, params=params)
     except Exception:
